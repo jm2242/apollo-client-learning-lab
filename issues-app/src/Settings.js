781ac9e17086484b56711f018a4c16b35671ca12
Jonathan Mares <jmares93@gmail.com>
2018-05-16 23:18:33 -0400

    remove unneeded node modules

---
+++ issues-app/src/Settings.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react"
+
+const Settings = () =>
+    <Fragment>
+        <h1>
+            View Your Personalized Settings Here
+        </h1>
+    </Fragment>
+
+export default Settings
